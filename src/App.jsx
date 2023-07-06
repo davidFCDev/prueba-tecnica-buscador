f16f0622b375395ac06a10e8d7bb9eed36d7e61a
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-06 17:15:54 +0200

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,59 @@
 import "./App.css";
+import debounce from "just-debounce-it";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 import { useSearch } from "./hooks/useSearch";
+import { useCallback, useState } from "react";
+
 
 function App() {
-  const { movies, getMovies, error } = useMovies();
-  const { search, updateSearch } = useSearch();
+	const [sort, setSort] = useState(false);
+	const { search, updateSearch, error } = useSearch();
+	const { movies, getMovies, loading } = useMovies({ search, sort });
 
-  const debouncedGetMovies = useCallback(
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const debouncedGetMovies = useCallback(
 		debounce(search => {
 			getMovies({ search });
 		}, 300),
 		[getMovies]
 	);
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    getMovies({ search });
-  };
+	const handleSubmit = event => {
+		event.preventDefault();
+		getMovies({ search });
+	};
 
-  const handleChange = event => {
+	const handleSort = () => {
+		setSort(!sort);
+	};
+
+	const handleChange = event => {
 		const newSearch = event.target.value;
 		updateSearch(newSearch);
 		debouncedGetMovies(newSearch);
 	};
 
-  return (
-    <div className="page">
-      <header>
-        <form onSubmit={handleSearch}>
-          <input
-            onChange={handleChange}
-            placeholder="Avengers, matrix..."
-            name="query"
-          />
-          <button>Search</button>
-        </form>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-      </header>
+	return (
+		<div className='page'>
+			<header>
+				<h1>Buscador de películas</h1>
+				<form className='form' onSubmit={handleSubmit}>
+					<input
+						name='query'
+						onChange={handleChange}
+						value={search}
+						placeholder='Avengers, matrix...'
+					/>
+					<input type='checkbox' onChange={handleSort} checked={sort} />
+					<button>Buscar</button>
+				</form>
+				{error && <p style={{ color: 'red' }}>{error}</p>}
+			</header>
 
-      <main>
-        <Movies movies={movies} />
-      </main>
-    </div>
-  );
+			<main>{loading ? <p>Loading...</p> : <Movies movies={movies} />}</main>
+		</div>
+	);
 }
 
 export default App;
