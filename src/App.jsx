0957e83f3d86ce1e70f5ebc46d6f6441536ab3e8
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-06 17:11:37 +0200

    añadido debounce

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
+import { useSearch } from "./hooks/useSearch";
 
 function App() {
-  const { movies } = useMovies();
+  const { movies, getMovies, error } = useMovies();
+  const { search, updateSearch } = useSearch();
+
+  const debouncedGetMovies = useCallback(
+		debounce(search => {
+			getMovies({ search });
+		}, 300),
+		[getMovies]
+	);
 
   const handleSearch = (event) => {
     event.preventDefault();
-    console.log("searching");
+    getMovies({ search });
   };
 
-  const handleChange = (event) => {};
+  const handleChange = event => {
+		const newSearch = event.target.value;
+		updateSearch(newSearch);
+		debouncedGetMovies(newSearch);
+	};
 
   return (
     <div className="page">
@@ -24,6 +36,7 @@ function App() {
           />
           <button>Search</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
---
+++ src/hooks/useSearch.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useSearch() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
